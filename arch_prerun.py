219fb0816341fcbef4e1e85806dcfaa6df10cf18
Syed Sazid Hossain Rezvi <65627729+SR-Hossain@users.noreply.github.com>
2023-03-03 23:36:22 +0600

    Update arch_prerun.py

--- arch_prerun.py
+++ arch_prerun.py
@@ -1,4 +1,6 @@
 s = open('firstOneTimeRun.sh').read()
 s = s.replace('apt-get', 'pamac')
+s = s.replace('sudo apt-get install flameshot -y', 'sudo pacman -S flameshot --noconfirm')
+s = s.replace('sudo apt-get install tesseract-ocr libtesseract-dev libleptonica-dev pkg-config -y', 'sudo pacman -S tesseract-data-eng --noconfirm')
 with open('firstOneTimeRun.sh', 'w') as f:
 	f.write(s)
