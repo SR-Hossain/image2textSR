2ec9f0e591989329d6fd64755904c46eeab5f156
Syed Sazid Hossain Rezvi <65627729+SR-Hossain@users.noreply.github.com>
2023-11-19 10:48:15 +0600

    Update firstOneTimeRun.sh

--- firstOneTimeRun.sh
+++ firstOneTimeRun.sh
@@ -1,7 +1,7 @@
 sudo apt install flameshot -y
 sudo apt install python3-pip
 sudo apt install tesseract-ocr libtesseract-dev libleptonica-dev pkg-config -y
-pip install pytesseract pyperclip --break-system-packages
+pip3 install pytesseract pyperclip
 sudo cp image2text.py /
 sudo cp image2textSR /usr/local/bin/
 sudo chmod +x /usr/local/bin/image2textSR
