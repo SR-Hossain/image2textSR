021d2d769715ff6664f405e4ffe618608ad4c949
SR-Hossain <sshr.rezvi@gmail.com>
2024-02-27 21:01:13 +0600

    version 2.0

---
+++ install.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+#Author : @SR-Hossain
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+ORANGE='\033[0;35m'
+RESET='\033[0m'
+
+clear
+echo "${YELLOW}⠀⠀⠀⠀⠀⠀⠀⢀⠀⠔⡀⠀⢀⠞⢰⠂⠀⠀⠀⠀⠀⠀⠀${RESET}"
+echo "${YELLOW}⠀⠀⠀⠀⠀⠀⠀⢸⠘⢰⡃⠔⠩⠤⠦⠤⢀⡀⠀⠀⠀⠀⠀${RESET}"
+echo "${YELLOW}⠀⠀⠀⠀⢀⠄⢒⠒⠺⠆⠈⠀⠀⢐⣂⠤⠄⡀⠯⠕⣒⣒⡀⠀${RESET}"
+echo "${YELLOW}⠀⠀⢐⡡⠔⠁⠆⠀⠀⠀⠀⠀⢀⠠⠙⢆⠀⠈⢁⠋⠥⣀⣀${RESET}"
+echo "${YELLOW}⠈⠉⠀⣰⠀⠀⠀⠀⡀⠀⢰⣆⢠⠠⢡⡀⢂⣗⣖⢝⡎⠉⠀${RESET}"
+echo "${YELLOW}⢠⡴⠛⡇⠀⠐⠀⡄⣡⢇⠸⢸⢸⡇⠂⡝⠌⢷⢫⢮⡜⡀⠀${RESET}"
+echo "${YELLOW}⠀⠀⠀⢰⣜⠘⡀⢡⠰⠳⣎⢂⣟⡎⠘⣬⡕⣈⣼⠢⠹⡟⠇${RESET}"
+echo "${YELLOW}⠀⠠⢋⢿⢳⢼⣄⣆⣦⣱⣿⣿⣿⣷⠬⣿⣿⣿⣿⠑⠵⠀⠀${RESET}"
+echo "${YELLOW}⠀⠀⠀⠀⡜⢩⣯⢝⡀⠁⠀⠙⠛⠛⠃⠀⠈⠛⠛⡿⠀⠀⠀⠀${RESET}"
+echo "${YELLOW}⠀⠀⠀⠀⠀⣿⠢⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀${RESET}"
+echo "${YELLOW}⠀⠀⠀⢀⣀⡇⠀⠑⠀⠀⠀⠀⠐⢄⠄⢀⡼⠃⠀⠀⠀⠀${RESET}"
+echo "${YELLOW}⠀⠀⠀⢸⣿⣷⣤⣀⠈⠲⡤⣀⣀⠀⡰⠋⠀⠀⠀⠀⠀⠀${RESET}"
+echo "${YELLOW}⠀⠀⠀⣼⣿⣿⣿⣿⣿⣶⣤⣙⣷⣅⡀⠀⠀⠀⠀⠀⠀⠀${RESET}"
+echo "${YELLOW}⠀⢀⣾⣿⣿⣿⣿⣻⢿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀${RESET}"
+echo "${YELLOW}⡠⠟⠁⠙⠟⠛⠛⢿⣿⣾⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀${RESET}"
+
+install() {    
+    echo "${BLUE}⠀⠀⠀⠀⠀⠀⠀⢀⠀⠔⡀⠀⢀⠞⢰⠂⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⠀⠀⠀⠀⢸⠘⢰⡃⠔⠩⠤⠦⠤⢀⡀⠀⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⠀⢀⠄⢒⠒⠺⠆⠈⠀⠀⢐⣂⠤⠄⡀⠯⠕⣒⣒⡀⠀${RESET}"
+    echo "${BLUE}⠀⠀⢐⡡⠔⠁⠆⠀⠀⠀⠀⠀⢀⠠⠙⢆⠀⠈⢁⠋⠥⣀⣀${RESET}"
+    echo "${BLUE}⠈⠉⠀⣰⠀⠀⠀⠀⡀⠀⢰⣆⢠⠠⢡⡀⢂⣗⣖⢝⡎⠉⠀${RESET}"
+    echo "${BLUE}⢠⡴⠛⡇⠀⠐⠀⡄⣡⢇⠸⢸⢸⡇⠂⡝⠌⢷⢫⢮⡜⡀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⢰⣜⠘⡀⢡⠰⠳⣎⢂⣟⡎⠘⣬⡕⣈⣼⠢⠹⡟⠇${RESET}"
+    echo "${BLUE}⠀⠠⢋⢿⢳⢼⣄⣆⣦⣱⣿⣿⣿⣷⠬⣿⣿⣿⣿⠑⠵⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⠀⡜⢩⣯⢝⡀⠁⠀⠙⠛⠛⠃⠀⠈⠛⠛⡿⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⠀⠀⣿⠢⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⢀⣀⡇⠀⠑⠀⠀⠀⠀⠐⢄⠄⢀⡼⠃⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⢸⣿⣷⣤⣀⠈⠲⡤⣀⣀⠀⡰⠋⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⣼⣿⣿⣿⣿⣿⣶⣤⣙⣷⣅⡀⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⢀⣾⣿⣿⣿⣿⣻⢿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⡠⠟⠁⠙⠟⠛⠛⢿⣿⣾⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "You selected Install"
+    echo "Select your Linux distro:"
+    echo "1. Debian e.g.Ubuntu, Pop OS"
+    echo "2. Arch Linux e.g. Manjaro"
+    read -p "Enter your choice: " choice
+
+    clear
+
+    echo "${BLUE}⠀⠀⠀⠀⠀⠀⠀⢀⠀⠔⡀⠀⢀⠞⢰⠂⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⠀⠀⠀⠀⢸⠘⢰⡃⠔⠩⠤⠦⠤⢀⡀⠀⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⠀⢀⠄⢒⠒⠺⠆⠈⠀⠀⢐⣂⠤⠄⡀⠯⠕⣒⣒⡀⠀${RESET}"
+    echo "${BLUE}⠀⠀⢐⡡⠔⠁⠆⠀⠀⠀⠀⠀⢀⠠⠙⢆⠀⠈⢁⠋⠥⣀⣀${RESET}"
+    echo "${BLUE}⠈⠉⠀⣰⠀⠀⠀⠀⡀⠀⢰⣆⢠⠠⢡⡀⢂⣗⣖⢝⡎⠉⠀${RESET}"
+    echo "${BLUE}⢠⡴⠛⡇⠀⠐⠀⡄⣡⢇⠸⢸⢸⡇⠂⡝⠌⢷⢫⢮⡜⡀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⢰⣜⠘⡀⢡⠰⠳⣎⢂⣟⡎⠘⣬⡕⣈⣼⠢⠹⡟⠇${RESET}"
+    echo "${BLUE}⠀⠠⢋⢿⢳⢼⣄⣆⣦⣱⣿⣿⣿⣷⠬⣿⣿⣿⣿⠑⠵⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⠀⡜⢩⣯⢝⡀⠁⠀⠙⠛⠛⠃⠀⠈⠛⠛⡿⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⠀⠀⣿⠢⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⢀⣀⡇⠀⠑⠀⠀⠀⠀⠐⢄⠄⢀⡼⠃⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⢸⣿⣷⣤⣀⠈⠲⡤⣀⣀⠀⡰⠋⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⠀⠀⣼⣿⣿⣿⣿⣿⣶⣤⣙⣷⣅⡀⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⠀⢀⣾⣿⣿⣿⣿⣻⢿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${BLUE}⡠⠟⠁⠙⠟⠛⠛⢿⣿⣾⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀${RESET}"
+
+    case $choice in
+        1)
+            echo "You selected Ubuntu"
+            sudo apt install flameshot python3-pip tesseract-ocr libtesseract-dev libleptonica-dev pkg-config -y
+            pip3 install pytesseract pyperclip
+            ;;
+        2)
+            echo "You selected Arch Linux"
+            sudo pacman -S tesseract-data-eng --noconfirm
+            pip install pytesseract pyperclip --break-system-packages
+            ;;
+        *)
+            echo "Invalid choice"
+            ;;
+    esac
+
+
+    sudo cp image2text.py /
+    sudo cp image2textSR /usr/local/bin/
+    sudo chmod +x /usr/local/bin/image2textSR
+    echo "Installation complete"
+    exit 0
+}
+
+about() {
+
+    echo "${GREEN}⠀⠀⠀⠀⠀⠀⠀⢀⠀⠔⡀⠀⢀⠞⢰⠂⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${GREEN}⠀⠀⠀⠀⠀⠀⠀⢸⠘⢰⡃⠔⠩⠤⠦⠤⢀⡀⠀⠀⠀⠀⠀${RESET}"
+    echo "${GREEN}⠀⠀⠀⠀⢀⠄⢒⠒⠺⠆⠈⠀⠀⢐⣂⠤⠄⡀⠯⠕⣒⣒⡀⠀${RESET}"
+    echo "${GREEN}⠀⠀⢐⡡⠔⠁⠆⠀⠀⠀⠀⠀⢀⠠⠙⢆⠀⠈⢁⠋⠥⣀⣀${RESET}"
+    echo "${GREEN}⠈⠉⠀⣰⠀⠀⠀⠀⡀⠀⢰⣆⢠⠠⢡⡀⢂⣗⣖⢝⡎⠉⠀${RESET}"
+    echo "${GREEN}⢠⡴⠛⡇⠀⠐⠀⡄⣡⢇⠸⢸⢸⡇⠂⡝⠌⢷⢫⢮⡜⡀⠀${RESET}"
+    echo "${GREEN}⠀⠀⠀⢰⣜⠘⡀⢡⠰⠳⣎⢂⣟⡎⠘⣬⡕⣈⣼⠢⠹⡟⠇${RESET}"
+    echo "${GREEN}⠀⠠⢋⢿⢳⢼⣄⣆⣦⣱⣿⣿⣿⣷⠬⣿⣿⣿⣿⠑⠵⠀⠀${RESET}"
+    echo "${GREEN}⠀⠀⠀⠀⡜⢩⣯⢝⡀⠁⠀⠙⠛⠛⠃⠀⠈⠛⠛⡿⠀⠀⠀⠀${RESET}"
+    echo "${GREEN}⠀⠀⠀⠀⠀⣿⠢⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀${RESET}"
+    echo "${GREEN}⠀⠀⠀⢀⣀⡇⠀⠑⠀⠀⠀⠀⠐⢄⠄⢀⡼⠃⠀⠀⠀⠀${RESET}"
+    echo "${GREEN}⠀⠀⠀⢸⣿⣷⣤⣀⠈⠲⡤⣀⣀⠀⡰⠋⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${GREEN}⠀⠀⠀⣼⣿⣿⣿⣿⣿⣶⣤⣙⣷⣅⡀⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${GREEN}⠀⢀⣾⣿⣿⣿⣿⣻⢿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${GREEN}⡠⠟⠁⠙⠟⠛⠛⢿⣿⣾⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀${RESET}"
+
+    echo ""
+    echo "This script installs the required packages for image2textSR"
+    echo "It will help you copy text from image using flameshot"
+    echo ""
+    echo "${BLUE}The author is @SR-Hossain"
+    echo "${GREEN}Syed Sazid Hossain Rezvi"
+    echo "${ORANGE}ssh.rezvi@gmail.com${RESET}"
+    exit 0
+}
+
+uninstall() {
+
+    echo "${RED}⠀⠀⠀⠀⠀⠀⠀⢀⠀⠔⡀⠀⢀⠞⢰⠂⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${RED}⠀⠀⠀⠀⠀⠀⠀⢸⠘⢰⡃⠔⠩⠤⠦⠤⢀⡀⠀⠀⠀⠀⠀${RESET}"
+    echo "${RED}⠀⠀⠀⠀⢀⠄⢒⠒⠺⠆⠈⠀⠀⢐⣂⠤⠄⡀⠯⠕⣒⣒⡀⠀${RESET}"
+    echo "${RED}⠀⠀⢐⡡⠔⠁⠆⠀⠀⠀⠀⠀⢀⠠⠙⢆⠀⠈⢁⠋⠥⣀⣀${RESET}"
+    echo "${RED}⠈⠉⠀⣰⠀⠀⠀⠀⡀⠀⢰⣆⢠⠠⢡⡀⢂⣗⣖⢝⡎⠉⠀${RESET}"
+    echo "${RED}⢠⡴⠛⡇⠀⠐⠀⡄⣡⢇⠸⢸⢸⡇⠂⡝⠌⢷⢫⢮⡜⡀⠀${RESET}"
+    echo "${RED}⠀⠀⠀⢰⣜⠘⡀⢡⠰⠳⣎⢂⣟⡎⠘⣬⡕⣈⣼⠢⠹⡟⠇${RESET}"
+    echo "${RED}⠀⠠⢋⢿⢳⢼⣄⣆⣦⣱⣿⣿⣿⣷⠬⣿⣿⣿⣿⠑⠵⠀⠀${RESET}"
+    echo "${RED}⠀⠀⠀⠀⡜⢩⣯⢝⡀⠁⠀⠙⠛⠛⠃⠀⠈⠛⠛⡿⠀⠀⠀⠀${RESET}"
+    echo "${RED}⠀⠀⠀⠀⠀⣿⠢⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀${RESET}"
+    echo "${RED}⠀⠀⠀⢀⣀⡇⠀⠑⠀⠀⠀⠀⠐⢄⠄⢀⡼⠃⠀⠀⠀⠀${RESET}"
+    echo "${RED}⠀⠀⠀⢸⣿⣷⣤⣀⠈⠲⡤⣀⣀⠀⡰⠋⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${RED}⠀⠀⠀⣼⣿⣿⣿⣿⣿⣶⣤⣙⣷⣅⡀⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${RED}⠀⢀⣾⣿⣿⣿⣿⣻⢿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀${RESET}"
+    echo "${RED}⡠⠟⠁⠙⠟⠛⠛⢿⣿⣾⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀${RESET}"
+
+    echo "You selected Uninstall"
+    sudo rm /usr/local/bin/image2textSR
+    sudo rm /image2text.py
+    echo ""
+    echo "${RED}Uninstallation complete${RESET}"
+    exit 0
+}
+
+echo "${BLUE}1. Install ${RESET}"
+echo "${GREEN}2. About${RESET}"
+echo "${RED}3. Uninstall${RESET}"
+echo ""
+echo "Select one ${BLUE}(default 1)${RESET}: "
+read choice
+
+clear
+
+case $choice in
+    1)
+        install
+        ;;
+    2)
+        about
+        ;;
+    3)
+        uninstall
+        ;;
+    *)
+        echo "Invalid choice"
+        ;;
+esac
